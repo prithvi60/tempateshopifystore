7dc0dbd307bdbc9312cf886d40e07790cf8b34cf
Prithvi <prithvin19@gmail.com>
2023-09-06 12:30:49 +0530

    test

--- components/cart/index.tsx
+++ components/cart/index.tsx
@@ -1,9 +1,11 @@
 import { getCart } from 'lib/shopify';
-import { cookies } from 'next/headers';
+// import { cookies } from 'next/headers';
 import CartModal from './modal';
 
 export default async function Cart() {
-  const cartId = cookies().get('cartId')?.value;
+  const cartId = 
+  // cookies().get('cartId')?.value
+  null;
   let cart;
 
   if (cartId) {
